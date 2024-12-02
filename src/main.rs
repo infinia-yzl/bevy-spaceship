58545c31d6b02b770d3d0a80f6d4f0782f3e5935
infinia-yzl <dev@infinia.space>
2024-12-02 13:18:09 +0800

    Spaceship game init

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,38 @@
 use bevy::prelude::*;
 
+#[derive(Component, Debug)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component, Debug)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
 fn main() {
     App::new()
+        .add_systems(Startup, spawn_spaceship)
+        .add_systems(Update, (print_position, update_position))
         .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
         .run();
 }
 
-pub struct HelloPlugin;
+fn spawn_spaceship(mut commands: Commands) {
+    commands.spawn((Position { x: 0., y: 0. }, Velocity { x: 1., y: 1. }));
+}
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
-        app.add_systems(Startup, add_people);
-        app.add_systems(Update, (update_people, greet_people).chain());
+fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
+    for (velocity, mut position) in query.iter_mut() {
+        position.x += velocity.x;
+        position.y += velocity.y;
     }
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if (timer.0.tick(time.delta())).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
-        }
+fn print_position(query: Query<(Entity, &Position)>) {
+    for (entity, position) in query.iter() {
+        info!("Entity {:?} is at position {:?}", entity, position);
     }
 }
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-    for mut name in &mut query {
-        if name.0 == "Elaina Proctor" {
-            name.0 = "Elaina Hume".to_string();
-            break; // We don't need to change any other names.
-        }
-    }
-}
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
