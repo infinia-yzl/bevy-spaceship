3628c9967be8bd24c3c21d845db1d4d712252597
infinia-yzl <dev@infinia.space>
2024-12-03 12:24:25 +0800

    Spaceship game EP 3: Forward Backward movement

--- src/spaceship.rs
+++ src/spaceship.rs
@@ -1,27 +1,54 @@
+use crate::asset_loader::SceneAssets;
 use crate::movement::{Acceleration, MovingObjectBundle, Velocity};
-use bevy::app::{App, Plugin, Startup};
+use bevy::app::{App, Plugin};
 use bevy::math::Vec3;
 use bevy::prelude::*;
-use crate::asset_loader::SceneAssets;
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
 const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+const SPACESHIP_SPEED: f32 = 25.0;
+const SPACESHIP_ROTATION_SPEED: f32 = 2.5;
+const SPACESHIP_ROLL_SPEED: f32 = 2.5;
+
+#[derive(Component, Debug)]
+pub struct Spaceship;
 
 pub struct SpaceshipPlugin;
 
 impl Plugin for SpaceshipPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PostStartup, spawn_spaceship);
+        app.add_systems(PostStartup, spawn_spaceship)
+            .add_systems(Update, spaceship_movement_controls);
     }
 }
 
 fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
-    commands.spawn(MovingObjectBundle {
-        velocity: Velocity {
-            value: STARTING_VELOCITY,
+    commands.spawn((
+        MovingObjectBundle {
+            velocity: Velocity::new(Vec3::ZERO),
+            acceleration: Acceleration::new(Vec3::ZERO),
+            transform: Transform::from_translation(STARTING_TRANSLATION),
+            model: SceneRoot(scene_assets.spaceship.clone()),
         },
-        acceleration: Acceleration::new(Vec3::ZERO),
-        transform: Transform::from_translation(STARTING_TRANSLATION),
-        model: SceneRoot(scene_assets.spaceship.clone()),
-    });
+        Spaceship,
+    ));
+}
+
+fn spaceship_movement_controls(
+    mut query: Query<(&mut Transform, &mut Velocity), With<Spaceship>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    let (mut transform, mut velocity) = query.single_mut();
+    let mut rotation = 0.0;
+    let mut roll = 0.0;
+    let mut movement = 0.0;
+
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        movement = -SPACESHIP_SPEED;
+    } else if keyboard_input.pressed(KeyCode::KeyW) {
+        movement = SPACESHIP_SPEED;
+    }
+
+    velocity.value = -transform.forward() * movement;
 }
